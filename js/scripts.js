b3a2c649ed612b00cbc0026bb90f688440d9555c
cristianhdez23 <cristianhernandez.ch878@gmail.com>
2024-04-18 19:16:18 +0200

    18 april 2024, init i18n

--- js/scripts.js
+++ js/scripts.js
@@ -903,3 +903,55 @@ $('[data-carousel="swiper"]').each(function () {
         var init = new Swiper(initID, conf);
     };
 });
+
+
+/* ===============================  i18n  =============================== */
+
+// Function to update content based on selected language
+function updateContent(langData) {
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        element.textContent = langData[key];
+    });
+}
+
+// Function to set the language preference
+function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang);
+    location.reload();
+}
+
+// Function to fetch language data
+async function fetchLanguageData(lang) {
+    const response = await fetch(`languages/${lang}.json`);
+    return response.json();
+}
+
+// Function to change language
+async function changeLanguage(lang) {
+    await setLanguagePreference(lang);
+    
+    const langData = await fetchLanguageData(lang);
+    updateContent(langData);
+}
+
+function detectLanguage(text) {
+    const upperText = text.toUpperCase();
+
+    switch (upperText) {
+        case "ES":
+            return "es";
+        case "EN":
+            return "en";
+        default:
+            return "en";
+    }
+}
+
+// Call updateContent() on page load
+window.addEventListener('DOMContentLoaded', async () => {
+    const userPreferredLanguage = detectLanguage(localStorage.getItem('language')) || 'en';
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    console.log('DOMContentLoaded');
+    updateContent(langData);
+});
